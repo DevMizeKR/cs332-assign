d07120c1063d5f3f8864916e0e6abb62373098f1
Temp_Name <hcg082743@gmail.com>
2024-10-08 00:03:17 +0900

    [Feat] Implemented Decode Part

--- patmat/src/main/scala/patmat/Huffman.scala
+++ patmat/src/main/scala/patmat/Huffman.scala
@@ -157,7 +157,16 @@ object Huffman {
    * This function decodes the bit sequence `bits` using the code tree `tree` and returns
    * the resulting list of characters.
    */
-  def decode(tree: CodeTree, bits: List[Bit]): List[Char] = ???
+  def decode(tree: CodeTree, bits: List[Bit]): List[Char] = {
+    val originTree = tree
+    def decodeRecursion(tree: CodeTree, bits: List[Bit]): List[Char] = (tree, bits) match {
+      case (Leaf(char, _), List()) => List(char) // Leaf && Last Character
+      case (Leaf(char, _), _) => char :: decodeRecursion(originTree, bits) // Leaf && Remaining Bits
+      case (Fork(left, right, chars, _), 0 :: _) => decodeRecursion(left, bits.tail) // Fork && Bit is 0
+      case (Fork(left, right, chars, _), 1 :: _) => decodeRecursion(right, bits.tail) // Fork && Bit is 1
+    }
+    decodeRecursion(tree, bits)
+  }
 
   /**
    * A Huffman coding tree for the French language.
@@ -175,7 +184,7 @@ object Huffman {
   /**
    * Write a function that returns the decoded secret
    */
-  def decodedSecret: List[Char] = ???
+  def decodedSecret: List[Char] = decode(frenchCode, secret)
 
 
 
